10e1f8a346bfc0a65b0c74460a156d5c0fc4470c
lann95081 <lann95081@gmail.com>
2022-12-21 16:52:17 +0700

    up

---
+++ ss6_7_jpa/exercise/blog/src/main/java/com/example/blog/service/IBlogService.java
@@ -0,0 +1,17 @@
+package com.example.blog.service;
+
+import com.example.blog.model.Blog;
+
+import java.util.List;
+
+public interface IBlogService {
+    List<Blog> findAll();
+
+    void save(Blog blog);
+
+    Blog findById(int id);
+
+    void update(Blog blog);
+
+    void remove(int id);
+}
---
+++ ss6_7_jpa/exercise/blog/src/main/java/com/example/blog/service/impl/BlogService.java
@@ -0,0 +1,41 @@
+package com.example.blog.service.impl;
+
+import com.example.blog.model.Blog;
+import com.example.blog.repository.IBlogRepository;
+import com.example.blog.service.IBlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BlogService implements IBlogService {
+
+    @Autowired
+    private IBlogRepository iBlogRepository;
+
+    @Override
+    public List<Blog> findAll() {
+        return iBlogRepository.findAll();
+    }
+
+    @Override
+    public void save(Blog blog) {
+        iBlogRepository.save(blog);
+    }
+
+    @Override
+    public Blog findById(int id) {
+        return iBlogRepository.findById(id);
+    }
+
+    @Override
+    public void update(Blog blog) {
+        iBlogRepository.save(blog);
+    }
+
+    @Override
+    public void remove(int id) {
+        iBlogRepository.deleteById(id);
+    }
+}
