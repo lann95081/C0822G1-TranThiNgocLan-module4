cf9548d091ba403280abe145e425196f5903ed27
lann95081 <lann95081@gmail.com>
2022-12-23 20:36:05 +0700

    up

---
+++ ss8_validation/exercise/music_validate/src/main/java/com/example/repository/IMusicRepository.java
@@ -0,0 +1,8 @@
+package com.example.repository;
+
+import com.example.model.Music;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IMusicRepository extends JpaRepository<Music, Integer> {
+    Music findById(int id);
+}
---
+++ ss8_validation/exercise/music_validate/src/main/java/com/example/service/IMusicService.java
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import com.example.model.Music;
+
+import java.util.List;
+
+public interface IMusicService {
+    List<Music> findAll();
+
+    void save(Music music);
+
+    Music findById(int id);
+
+    void update(Music music);
+}
