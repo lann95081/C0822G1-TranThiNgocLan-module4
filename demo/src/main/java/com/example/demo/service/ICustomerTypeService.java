c17afa7b570025ea2f2ecfead3f80d81884264db
lann95081 <lann95081@gmail.com>
2023-01-02 23:06:57 +0700

    up

--- demo/src/main/java/com/example/demo/service/ICustomerTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.CustomerType;
-
-import java.util.List;
-
-public interface ICustomerTypeService {
-    List<CustomerType> findAll();
-}
