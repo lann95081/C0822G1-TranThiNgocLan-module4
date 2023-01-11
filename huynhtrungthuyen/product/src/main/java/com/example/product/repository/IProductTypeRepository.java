506bc48aa791c11fbf5042ed5277cde97dde6896
lann95081 <lann95081@gmail.com>
2023-01-11 15:41:02 +0700

    up

--- huynhtrungthuyen/product/src/main/java/com/example/product/repository/IProductTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.product.repository;
-
-import com.example.product.model.ProductType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProductTypeRepository extends JpaRepository<ProductType, Integer> {
-}
--- huynhtrungthuyen/product/src/main/java/com/example/product/service/IProductTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.service;
-
-import com.example.product.model.ProductType;
-
-import java.util.List;
-
-public interface IProductTypeService {
-    List<ProductType> findAll();
-}
